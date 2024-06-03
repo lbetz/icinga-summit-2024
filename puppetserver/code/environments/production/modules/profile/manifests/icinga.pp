6603851ffa6cc21afff39cc650de44902f6c33d9
Lennart Betz <lennart.betz@netways.de>
2024-06-03 14:44:38 +0200

    add step 9

--- puppetserver/code/environments/production/modules/profile/manifests/icinga.pp
+++ puppetserver/code/environments/production/modules/profile/manifests/icinga.pp
@@ -27,6 +27,19 @@ class profile::icinga (
             'log_duration' => 0,
           },
         },
+        'Service' => {
+          'icinga' => {
+            'host_name'        => $node,
+            'import'           => ['generic-service'],
+            'check_command'    => 'icinga',
+            'command_endpoint' => 'host_name',
+          },
+          'cluster zone' => {
+            'host_name'        => $node,
+            'import'           => ['generic-service'],
+            'check_command'    => 'cluster-zone',
+          },
+        },
       }
     } # agent
 
