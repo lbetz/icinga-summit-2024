77cb389a6dd509d2a5a06b2a4bbd76369b459c96
Lennart Betz <lennart.betz@netways.de>
2024-06-03 13:42:32 +0200

    add step 4

--- puppetserver/code/environments/production/modules/profile/manifests/icinga.pp
+++ puppetserver/code/environments/production/modules/profile/manifests/icinga.pp
@@ -7,6 +7,11 @@ class profile::icinga (
     'agent': {
       include icinga::repos
       include icinga::agent
+
+      $node        = $icinga::cert_name
+      $zone        = $node
+      $parent_zone = $icinga::agent::parent_zone
+      $target      = "/etc/icinga2/zones.d/${parent_zone}/${node}.conf"
     } # agent
 
     'worker': {
@@ -18,12 +23,19 @@ class profile::icinga (
         manage_extras => true,
       }
       include profile::icinga::server
+
+      $node        = $icinga::cert_name
+      $zone        = $icinga::server::zone
+      $target      = "/etc/icinga2/zones.d/${zone}/${node}.conf"
     } # server
   }
 
   $objects.each |String $type, Hash $objs| {
     notify { $type:
-      message => $objs,
+      message => $objs.reduce({}) |$memo, $obj| {
+        $memo + { $obj[0] => $obj[1] + {
+            'target' => $target }}
+      },
     }
   }
 }
